0ee08f22e6997b8151611169e6179d3b306d19fe
Ramin <riranmanesh05@gmail.com>
2026-09-18 21:46:00 +0330

    Update ALU.vhd

--- E1/ALU.vhd
+++ E1/ALU.vhd
@@ -1,5 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
 ENTITY ALU IS
     PORT (
@@ -15,10 +16,12 @@ ENTITY ALU IS
 END ENTITY ALU;
 
 ARCHITECTURE struct OF ALU IS
-
-    SIGNAL Carry : STD_LOGIC <= '0';
     SIGNAL Temp : STD_LOGIC_VECTOR(31 DOWNTO 0) <= (OTHERS => '0');
-
+    SIGNAL SignedTempA : signed(31 DOWNTO 0) <= (OTHERS => '0');
+    SIGNAL SignedTempB : signed(31 DOWNTO 0) <= (OTHERS => '0');
+    SIGNAL UnsignedTempA : unsigned(31 DOWNTO 0) <= (OTHERS => '0');
+    SIGNAL UnsignedTempB : unsigned(31 DOWNTO 0) <= (OTHERS => '0');
+    SIGNAL TempS : unsigned(32 DOWNTO 0) <= (OTHERS => '0');
 BEGIN
     CASE S IS
         WHEN "00000" =>
@@ -43,6 +46,56 @@ BEGIN
             Result <= A XNOR B;
 
         WHEN "00111" =>
+            IF A = B THEN
+                Result <= (0 => '1', OTHERS => '0');
+            ELSE
+                Result <= (0 => '0', OTHERS => '0');
+            END IF;
+
+        WHEN "01000" =>
+            IF A = B THEN
+                Result <= (0 => '0', OTHERS => '0');
+            ELSE
+                Result <= (0 => '1', OTHERS => '0');
+            END IF;
+
+        WHEN "01001" =>
+            SignedTempA <= signed(A);
+            SignedTempB <= signed(B);
+            IF SignedTempA < SignedTempB THEN
+                Result <= (0 => '1', OTHERS => '0');
+            ELSE
+                Result <= (0 => '0', OTHERS => '0');
+            END IF;
+
+        WHEN "01010" =>
+            SignedTempA <= signed(A);
+            SignedTempB <= signed(B);
+            IF SignedTempA > SignedTempB THEN
+                Result <= (0 => '1', OTHERS => '0');
+            ELSE
+                Result <= (0 => '0', OTHERS => '0');
+            END IF;
+
+        WHEN "01011" =>
+            SignedTempA <= signed(A);
+            SignedTempB <= signed(B);
+
+            IF SignedTempA <= SignedTempB THEN
+                Result <= (0 => '1', OTHERS => '0');
+            ELSE
+                Result <= (0 => '0', OTHERS => '0');
+            END IF;
+
+        WHEN "01100" =>
+            SignedTempA <= signed(A);
+            SignedTempB <= signed(B);
+
+            IF SignedTempA >= SignedTempB THEN
+                Result <= (0 => '1', OTHERS => '0');
+            ELSE
+                Result <= (0 => '0', OTHERS => '0');
+            END IF;
 
         WHEN OTHERS =>
             Result <= A;
