38a58eaa447f1cdf4b0e9f229ae843d49c459476
Ramin <riranmanesh05@gmail.com>
2026-09-19 06:25:56 +0330

    Update ALU.vhd

--- E1/ALU.vhd
+++ E1/ALU.vhd
@@ -43,157 +43,152 @@ END ENTITY ALU;
 -- Architecture of ALU
 ARCHITECTURE struct OF ALU IS
     -- Signal for logic Operations
-    SIGNAL TempNOT : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempAND : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempXOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempNAND : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempNOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempXNOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultNOT : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultAND : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultXOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultNAND : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultNOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultXNOR : STD_LOGIC_VECTOR(63 DOWNTO 0);
 
     -- Signal for compare operations
-    SIGNAL TempEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempNEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempL : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempG : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempLEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempGEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultNEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultL : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultG : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultLEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultGEQ : STD_LOGIC_VECTOR(63 DOWNTO 0);
 
     -- Signal for arithmetic Operations
-    SIGNAL TempADD : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempSUB : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempINC : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempDEC : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempNEG : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL TempADD : unsigned(64 DOWNTO 0);
+    SIGNAL TempSUB : unsigned(64 DOWNTO 0);
+    SIGNAL TempINC : unsigned(64 DOWNTO 0);
+    SIGNAL TempDEC : unsigned(64 DOWNTO 0);
+    SIGNAL TempNEG : unsigned(64 DOWNTO 0);
+    SIGNAL ResultADD : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultSUB : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultINC : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultDEC : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultNEG : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL CoutADD : STD_LOGIC;
+    SIGNAL CoutSUB : STD_LOGIC;
+    SIGNAL CoutINC : STD_LOGIC;
+    SIGNAL CoutDEC : STD_LOGIC;
+    SIGNAL CoutNEG : STD_LOGIC;
+    SIGNAL OverflowADD : STD_LOGIC;
+    SIGNAL OverflowSUB : STD_LOGIC;
+    SIGNAL OverflowINC : STD_LOGIC;
+    SIGNAL OverflowDEC : STD_LOGIC;
+    SIGNAL OverflowNEG : STD_LOGIC;
 
     -- Signal for shift and routing Operations
-    SIGNAL TempSHL : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL CoutSHL : STD_LOGIC;
-    SIGNAL TempSHR : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL CoutSHR : STD_LOGIC;
-    SIGNAL TempASR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultSHL : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultSHR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultASR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultROL : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL ResultROR : STD_LOGIC_VECTOR(63 DOWNTO 0);
     SIGNAL CoutASR : STD_LOGIC;
-    SIGNAL TempROL : STD_LOGIC_VECTOR(63 DOWNTO 0);
-    SIGNAL TempROR : STD_LOGIC_VECTOR(63 DOWNTO 0);
+    SIGNAL CoutSHL : STD_LOGIC;
+    SIGNAL CoutSHR : STD_LOGIC;
 
-    SIGNAL USTempA : unsigned(63 DOWNTO 0);
-    SIGNAL USTempB : unsigned(63 DOWNTO 0);
-    SIGNAL USTempS : unsigned(64 DOWNTO 0);
+    SIGNAL USTemp : unsigned(63 DOWNTO 0);
 BEGIN
+    ------------------------------------------------------------------------------------------
     -- Logic Operations
     -- NOT operation
-    TempNOT <= NOT A;
+    ResultNOT <= NOT A;
     -- AND operation
-    TempAND <= A AND B;
+    ResultAND <= A AND B;
     -- OR operation
-    TempOR <= A OR B;
+    ResultOR <= A OR B;
     -- XOR operation
-    TempXOR <= A XOR B;
+    ResultXOR <= A XOR B;
     -- NAND operation
-    TempNAND <= A NAND B;
+    ResultNAND <= A NAND B;
     -- NOR operation
-    TempNOR <= A NOR B;
+    ResultNOR <= A NOR B;
     -- XNOR operation
-    TempXNOR <= A XNOR B;
+    ResultXNOR <= A XNOR B;
+    ------------------------------------------------------------------------------------------
 
-    -- Signal for compare operations
-    -- EQ and NEQ compare operation
-    IF A = B THEN
-        TempEQ <= (0 => '1', OTHERS => '0');
-        TempNEQ <= (OTHERS => '0');
-    ELSE
-        TempEQ <= (OTHERS => '0');
-        TempNEQ <= (0 => '1', OTHERS => '0');
-    END IF;
+    ------------------------------------------------------------------------------------------
+    -- Compare operations
+    -- EQ compare operation
+    ResultEQ <= (0 => '1', OTHERS => '0') WHEN A = B ELSE
+        (OTHERS => '0');
+
+    -- NEQ compare operation
+    ResultNEQ <= (OTHERS => '0') WHEN A = B ELSE
+        (0 => '1', OTHERS => '0');
 
     -- A < B compare operation
-    IF signed(A) < signed(B) THEN
-        TempL <= (0 => '1', OTHERS => '0');
-    ELSE
-        TempL <= (OTHERS => '0');
-    END IF;
+    ResultL <= (0 => '1', OTHERS => '0') WHEN signed(A) < signed(B) ELSE
+        (OTHERS => '0');
 
     -- A > B compare operation
-    IF signed(A) > signed(B) THEN
-        TempG <= (0 => '1', OTHERS => '0');
-    ELSE
-        TempG <= (OTHERS => '0');
-    END IF;
+    ResultG <= (0 => '1', OTHERS => '0') WHEN signed(A) > signed(B) ELSE
+        (OTHERS => '0');
 
     -- A <= B compare operation
-    IF signed(A) <= signed(B) THEN
-        TempLEQ <= (0 => '1', OTHERS => '0');
-    ELSE
-        TempL <= (OTHERS => '0');
-    END IF;
+    ResultLEQ <= (0 => '1', OTHERS => '0') WHEN signed(A) <= signed(B) ELSE
+        (OTHERS => '0');
 
     -- A >= B compare operation
-    IF signed(A) >= signed(B) THEN
-        TempGEQ <= (0 => '1', OTHERS => '0');
-    ELSE
-        TempGEQ <= (OTHERS => '0');
-    END IF;
+    ResultGEQ <= (0 => '1', OTHERS => '0') WHEN signed(A) >= signed(B) ELSE
+        (OTHERS => '0');
+    ------------------------------------------------------------------------------------------
 
+    ------------------------------------------------------------------------------------------
+    -- Arithmetic operations
+    -- ADD operation
+    ResultADD <= STD_LOGIC_VECTOR((unsigned('0' & A) + unsigned('0' & B))(63 DOWNTO 0));
+    CoutADD <= unsigned('0' & A) + unsigned('0' & B)(64);
+    OverflowADD <= (NOT A(63) AND NOT B(63) AND ResultADD(63))
+        OR (A(63) AND B(63) AND NOT ResultADD(63));
 
+    -- SUB operation
+    ResultSUB <= STD_LOGIC_VECTOR((unsigned('0' & A) + unsigned('0' & (NOT B)) + to_unsigned(1, 65))(63 DOWNTO 0));
+    CoutSUB <= (unsigned('0' & A) + unsigned('0' & (NOT B)) + to_unsigned(1, 65))(64);
+    OverflowSUB <= (NOT A(63) AND B(63) AND ResultSUB(63))
+        OR (A(63) AND NOT B(63) AND NOT ResultSUB(63));
+
+    -- INC operation
+    ResultINC <= STD_LOGIC_VECTOR((unsigned('0' & A) + to_unsigned(1, 65))(63 DOWNTO 0));
+    CoutINC <= (unsigned('0' & A) + to_unsigned(1, 65))(64);
+    OverflowINC <= (NOT A(63) AND ResultINC(63));
+
+    -- DEC operation
+    USTemp <= (OTHERS => '1');
+    ResultDEC <= STD_LOGIC_VECTOR((unsigned('0' & A) + unsigned('0' & USTemp))(63 DOWNTO 0));
+    CoutDEC <= (unsigned('0' & A) + unsigned('0' & USTemp))(64);
+    OverflowDEC <= (NOT A(63) AND ResultDEC(63));
+
+    -- NEG operation
+    ResultNEG <= STD_LOGIC_VECTOR((unsigned('0' & (NOT A)) + to_unsigned(1, 65))(63 DOWNTO 0));
+    CoutNEG <= (unsigned('0' & (NOT A)) + to_unsigned(1, 65))(64);
+    OverflowNEG <= '1' WHEN A = x"8000000000000000" ELSE
+        '0';
+    ------------------------------------------------------------------------------------------
+
+    ------------------------------------------------------------------------------------------
     -- Shift and Routing operations
     -- SHL operation
-    TempSHL <= A(62 DOWNTO 0) & '0';
+    ResultSHL <= A(62 DOWNTO 0) & '0';
     CoutSHL <= A(63);
 
     -- SHR operation
-    TempSHR <= '0' & A(63 DOWNTO 1);
+    ResultSHR <= '0' & A(63 DOWNTO 1);
     CoutSHR <= A(0);
 
     -- ASR operation
-    TempASR <= A(63) & A(63 DOWNTO 1);
+    ResultASR <= A(63) & A(63 DOWNTO 1);
     CoutASR <= A(0);
 
     -- ROL operation 
-    TempROL <= A(62 DOWNTO 0) & A(63);
+    ResultROL <= A(62 DOWNTO 0) & A(63);
 
     -- ROR operation
-    TempROR <= A(0) & A(63 DOWNTO 1);
+    ResultROR <= A(0) & A(63 DOWNTO 1);
+    ------------------------------------------------------------------------------------------
 
-    CASE S IS
-            -- ADD operation
-        WHEN "10000" =>
-            USTempS <= unsigned('0' & A) + unsigned('0' & B);
-            Result <= STD_LOGIC_VECTOR(USTempS(63 DOWNTO 0));
-            Cout <= USTempS(64);
-            Overflow <= (NOT A(63) AND NOT B(63) AND Result(63))
-                OR (A(63) AND B(63) AND NOT Result(63));
-
-            -- SUB operation
-        WHEN "10001" =>
-            USTempS <= unsigned('0' & A) + unsigned('0' & (NOT B)) + to_unsigned(1, 65);
-            Result <= STD_LOGIC_VECTOR(USTempS(63 DOWNTO 0));
-            Cout <= USTempS(64);
-            Overflow <= (NOT A(63) AND B(63) AND Result(63))
-                OR (A(63) AND NOT B(63) AND NOT Result(63));
-
-            -- INC operation
-        WHEN "100010" =>
-            USTempS <= unsigned('0' & A) + to_unsigned(1, 65);
-            Result <= STD_LOGIC_VECTOR(USTempS(63 DOWNTO 0));
-            Cout <= USTempS(64);
-            Overflow <= (NOT A(63) AND Result(63));
-
-            -- DEC operation
-        WHEN "10011" =>
-            USTempA <= (OTHERS => 1)
-                USTempS <= unsigned('0' & A) + unsigned('0' & USTempA);
-            Result <= STD_LOGIC_VECTOR(USTempS(63 DOWNTO 0));
-            Cout <= USTempS(64);
-            Overflow <= (NOT A(63) AND Result(63));
-
-            -- NEG operation
-        WHEN "10100" =>
-            USTempS <= unsigned('0' & (NOT A)) + to_unsigned(1, 65);
-            Result <= STD_LOGIC_VECTOR(USTempS(63 DOWNTO 0));
-            Cout <= USTempS(64);
-            Overflow <= (NOT A(63) AND Result(63));
-            -- don't cares (Buffer)
-        WHEN OTHERS =>
-            Result <= A;
-    END CASE;
 END struct;
