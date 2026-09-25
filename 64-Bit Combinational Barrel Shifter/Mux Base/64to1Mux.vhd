7ed90006cb2802936f616f47fe62df05ad4b336f
Ramin <riranmanesh05@gmail.com>
2026-09-25 23:53:59 +0330

    Update 64to1Mux.vhd

--- 64-Bit Combinational Barrel Shifter/Mux Base/64to1Mux.vhd
+++ 64-Bit Combinational Barrel Shifter/Mux Base/64to1Mux.vhd
@@ -1,3 +1,10 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
-Use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+ENTITY 64to1Mux IS
+    PORT (
+        Inputs : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
+        S : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+        O : OUT STD_LOGIC;
+    );
+END ENTITY 64to1Mux;
